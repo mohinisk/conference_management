b591752757a1c2f44f2b9c8d4963e3c3b882b90f
user <mohini.k@indictranstech.com>
2017-02-22 19:58:11 +0530

    [Check availability mandatory in conference booking]

--- conference_management/conference_management/doctype/conference_booking/conference_booking.js
+++ conference_management/conference_management/doctype/conference_booking/conference_booking.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2016, GoElite and contributors
 // For license information, please see license.txt
 
+var check=1
 frappe.ui.form.on('Conference booking', {
+	
 	onload: function(frm){
+		//var check_availability_flag=0
 		console.log(cur_frm.doc.conference);
 		if(frm.doc.__islocal) 
 		{ 
@@ -109,20 +112,7 @@ frappe.ui.form.on('Conference booking', {
 							console.log("c",frm.doc.city)
 							console.log("a",frm.doc.area)
 							console.log("f",frm.doc.facility)
-							/*cur_frm.set_df_property("conference","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("area","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("city","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("facility","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("building","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("location","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("date","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("from_time","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("to_time","read_only",cur_frm.doc.__islocal);*/
-							/*cur_frm.set_df_property("attendees","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("send_invite","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("agenda","read_only",cur_frm.doc.__islocal);
-							cur_frm.set_df_property("pantry_service","read_only",cur_frm.doc.__islocal);*/
-
+							
 							if(frm.doc.city==undefined && frm.doc.area==undefined && frm.doc.facility==undefined){
 								console.log("IIFIF")
 								cur_frm.set_value("location",User_details.location);
@@ -152,11 +142,6 @@ frappe.ui.form.on('Conference booking', {
 								cur_frm.set_df_property("from_time","read_only",cur_frm.doc.__islocal);
 								cur_frm.set_df_property("to_time","read_only",cur_frm.doc.__islocal);
 							}
-
-							//cur_frm.set_df_property("attendees","read_only",cur_frm.doc.__islocal);
-							//cur_frm.set_df_property("send_invite","read_only",cur_frm.doc.__islocal);
-							//cur_frm.set_df_property("agenda","read_only",cur_frm.doc.__islocal);
-							//cur_frm.set_df_property("pantry_service","read_only",cur_frm.doc.__islocal);
 						}	
 								
 				}
@@ -201,32 +186,50 @@ frappe.ui.form.on('Conference booking', {
 		/* Function for checking availability of conference for selected time and date 
 		   and conference on conference booking.
 		*/
-		frappe.call({
-				method :"conference_management.conference_management.doctype.conference_booking.conference_booking.check_availability",
-				args:{
-			 			"date":frm.doc.date,
-						"from_time":frm.doc.from_time,
-						"to_time":frm.doc.to_time,
-						"conference":frm.doc.conference
-			 	},
-				callback: function(r) {
-					cur_frm.set_value("availability",r.message);
-				}
-			});
+		console.log("check");
+		console.log(check);
+		if(!frm.doc.conference){
+			frappe.msgprint(__("Please select conference"))
+		}
+		if(!frm.doc.to_time){
+			frappe.msgprint(__("Please enter to time"))
+		}
+		if(!frm.doc.from_time){
+			frappe.msgprint(__("Please enter from time"))
+		}
+		if(!frm.doc.date){
+			frappe.msgprint(__("Please enter date"))
+		}
+		else {
+			frappe.call({
+					method :"conference_management.conference_management.doctype.conference_booking.conference_booking.check_availability",
+					args:{
+				 			"date":frm.doc.date,
+							"from_time":frm.doc.from_time,
+							"to_time":frm.doc.to_time,
+							"conference":frm.doc.conference
+				 	},
+					callback: function(r) {
+						cur_frm.set_value("availability",r.message);
+						check=0;
+					}
+				});
+			
+		}
 	},
 	booking:function(frm){
 		if(cur_frm.doc.booking=="search")
 		{
 			cur_frm.set_df_property("check_availability","hidden",1);
 		}
+	},
+	validate:function(frm){
+		console.log("checkkkkk",check);
+		if(check==1)
+		{
+			frappe.throw("Please fisrt check availability");
+		}
 	}
-	// validate:function(frm){
-
-
-		
-		
-		
-	// }
 
 	
 
